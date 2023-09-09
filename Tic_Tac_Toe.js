173c673f0b66e6a2ee0c6192849879997c7b761c
PaulDenis <78157298+PaulDenis@users.noreply.github.com>
2023-09-09 16:58:11 +0200

    spazio di gioco creato automticamente. Riesco a recuperare gli ID delle varie parti.

--- Tic_Tac_Toe.js
+++ Tic_Tac_Toe.js
@@ -1,8 +1,27 @@
 var clicked = false;
 var player = 0;
 
-document.getElementById("card_1").onclick = function () {
-  clicked = true;
-  document.getElementById("card_1").style.backgroundColor = "red";
-  console.log("Yes");
-};
+var cards = [];
+for (i=0; i < 9; i++) {
+  var div = document.createElement("div");
+  div.className = "game_grid";
+  div.setAttribute("id", "card_"+ i);
+  cards.push("card_" + i);
+  document.getElementById("game").appendChild(div);
+  // console.log(cards);
+}
+
+const tics = document.getElementsByClassName("game_grid")
+
+const card_pressed = e => {
+  console.log(e.target.id);
+}
+
+for (let tic of tics) {
+  tic.addEventListener("click", card_pressed);
+}
+
+
+
+
+
