a7440c55f6f90df1399f6fea6301e5c85c285688
PaulDenis <78157298+PaulDenis@users.noreply.github.com>
2023-09-10 22:26:02 +0200

    Added comments

--- Tic_Tac_Toe.js
+++ Tic_Tac_Toe.js
@@ -2,7 +2,7 @@ var player = 0;
 var clicked = false;
 var cards = [];
 
-
+/* This section creates the 9 squares to play tic-tac-toe on */
 for (i=0; i < 9; i++) {
   var div = document.createElement("div");
   div.className = "game_grid";
@@ -12,12 +12,15 @@ for (i=0; i < 9; i++) {
   //console.log(cards);
 }
 
-const tics = document.getElementsByClassName("game_grid")
+const tics = document.getElementsByClassName("game_grid") /* This is a list of the 9 squares on wich the game is played */
 
+
+/* This section is the logic that allow to click
+ the squares and to change them to wither X or O */
 const card_pressed = e => {
   // console.log(e.target.id);
   //console.log (e.target)
-  if (e.target.classList.contains("clicked") == false) {
+  if (e.target.classList.contains("clicked") == false) { /* Here i check if the square has alredy been clicked */
     if (player == false) {
       e.target.className = e.target.classList + " pressed clicked";
       player = !player;
@@ -29,6 +32,8 @@ const card_pressed = e => {
   }
 }
 
+/* Here i add a listener on all the squares, so when you click one
+  it will call the card_pressed function above */
 for (let tic of tics) {
   tic.addEventListener("click", card_pressed);
 }
