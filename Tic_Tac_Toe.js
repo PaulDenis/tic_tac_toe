985b653d01edcb6d688c901ac126e8c2e47dfb0f
PaulDenis <78157298+PaulDenis@users.noreply.github.com>
2023-09-10 17:15:08 +0200

    The game is made to work for 2 players. There is not yet a winning screen.

--- Tic_Tac_Toe.js
+++ Tic_Tac_Toe.js
@@ -1,20 +1,32 @@
-var clicked = false;
 var player = 0;
-
+var clicked = false;
 var cards = [];
+
+
 for (i=0; i < 9; i++) {
   var div = document.createElement("div");
   div.className = "game_grid";
   div.setAttribute("id", "card_"+ i);
-  cards.push("card_" + i);
+  cards.push({"clicked" : false , "card" : "card_" + i});
   document.getElementById("game").appendChild(div);
-  // console.log(cards);
+  //console.log(cards);
 }
 
 const tics = document.getElementsByClassName("game_grid")
 
 const card_pressed = e => {
-  console.log(e.target.id);
+  // console.log(e.target.id);
+  //console.log (e.target)
+  if (e.target.classList.contains("clicked") == false) {
+    if (player == false) {
+      e.target.className = e.target.classList + " pressed clicked";
+      player = !player;
+    } 
+    else {
+      e.target.className = e.target.classList + " pressed_enemy clicked";
+      player = !player;
+    }
+  }
 }
 
 for (let tic of tics) {
